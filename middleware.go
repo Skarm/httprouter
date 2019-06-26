102ed24cef9136656b17d9d57c69561b5d9d163e
Skarm <Skarm@users.noreply.github.com>
2019-06-26 17:28:26 +0500

    Add wrap handle middleware

--- middleware.go
+++ middleware.go
@@ -7,3 +7,25 @@ func (r *Router) Use(m ...middleware) *Router {
 	r.middlewares = append(m, r.middlewares...)
 	return r
 }
+
+// Wrap a given handle with the current stack from the result of Use() calls.
+func (r *Router) Wrap(fn Handle) Handle {
+	l := len(r.middlewares)
+	if l == 0 {
+		return fn
+	}
+
+	// There is at least one item in the list. Starting
+	// with the last item, create the handler to be
+	// returned:
+	var result Handle
+	result = r.middlewares[l-1](fn)
+
+	// Reverse through the stack for the remaining elements,
+	// and wrap the result with each layer:
+	for i := 0; i < (l - 1); i++ {
+		result = r.middlewares[l-(2+i)](result)
+	}
+
+	return result
+}
